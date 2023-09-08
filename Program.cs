be8f8ad292443a201ffc03448a4c8c2ed323487a
anhnt97 <anhntlinh@gmail.com>
2023-09-08 14:47:43 +0700

    Create project 20 Valid Parentheses

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace _20_Valid_Parentheses
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            bool isValid = IsValid("{()]}");
+
+            Console.WriteLine(isValid);
+        }
+
+        public static bool IsValid(string s)
+        {
+            if (s.Length % 2 != 0)
+                return false;
+
+            Stack<char> chars = new();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    chars.Push(')');
+                }
+                else if (s[i] == '[')
+                {
+                    chars.Push(']');
+                }
+                else if (s[i] == '{')
+                {
+                    chars.Push('}');
+                }
+                else if (s[i] == ')' || s[i] == ']' || s[i] == '}')
+                {
+                    if (chars.Pop() == s[i])
+                        continue;
+                    else
+                        return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
