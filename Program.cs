6e49ff6e084ae26ba817e7ac6b41bec61835c9c3
anhnt97 <anhntlinh@gmail.com>
2023-09-08 15:40:14 +0700

    Create empty project 21 merge two sorted lists

--- Program.cs
+++ Program.cs
@@ -5,31 +5,6 @@ namespace _20_Valid_Parentheses
     {
         public static void Main()
         {
-            bool isValid = IsValid("{[()]}");
-
-            Console.WriteLine(isValid);
-        }
-
-        public static bool IsValid(string s)
-        {
-            if (s.Length % 2 != 0)
-                return false;
-
-            Stack<char> chars = new();
-
-            for (int i = 0; i < s.Length; i++)
-            {
-                if (s[i] == '(')
-                    chars.Push(')');
-                else if (s[i] == '[')
-                    chars.Push(']');
-                else if (s[i] == '{')
-                    chars.Push('}');
-                else if (chars.Count <= 0 || chars.Pop() != s[i])
-                    return false;
-            }
-
-            return chars.Count <= 0;
         }
     }
 }
